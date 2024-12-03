e5e1065b8280384010b70d9b9f8a14074825ce85
Ivan Chernikov <chernikoviivan@gmail.com>
2024-12-03 17:43:12 +0100

    Implement 1st assignment

--- src/main/java/org/cherniva/App.java
+++ src/main/java/org/cherniva/App.java
@@ -1,13 +1,12 @@
 package org.cherniva;
 
+import org.cherniva.assignments.Asgn1;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Asgn1.solve();
     }
 }
---
+++ src/main/java/org/cherniva/assignments/Asgn1.java
@@ -0,0 +1,46 @@
+package org.cherniva.assignments;
+
+import java.util.*;
+
+public class Asgn1 {
+    private static int measureDistance(List<Integer> list1, List<Integer> list2) {
+        Collections.sort(list1);
+        Collections.sort(list2);
+        int distance = 0;
+        for (int i = 0; i < list1.size(); i++) {
+            distance += Math.abs(list1.get(i) - list2.get(i));
+        }
+        return distance;
+    }
+
+    private static int countSimilarNums(List<Integer> list1, List<Integer> list2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer n : list2) {
+            if (map.containsKey(n))
+                map.put(n, map.get(n) + 1);
+            else
+                map.put(n, 1);
+        }
+        int sum = 0;
+        for (Integer n : list1) {
+            if (map.get(n) != null) sum += map.get(n) * n;
+        }
+        return sum;
+    }
+
+    public static void solve() {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+
+            int n1 = Integer.parseInt(line.substring(0, line.indexOf(" ")));
+            int n2 = Integer.parseInt(line.substring(line.lastIndexOf(" ")).trim());
+            list1.add(n1);
+            list2.add(n2);
+        }
+        System.out.println(measureDistance(list1, list2));
+        System.out.println(countSimilarNums(list1, list2));
+    }
+}
